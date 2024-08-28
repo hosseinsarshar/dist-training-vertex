8cd2dd6772dc446158eca8ed4749f1bd0019fe65
bvrockwell <rockwellb@google.com>
2024-08-27 18:27:12 -0700

    clean up read me and configure checkpoint transfer

--- nemo/launch.sh
+++ nemo/launch.sh
@@ -37,7 +37,18 @@ if [ -z "$TRAIN_TYPE" ] || [ -z "$MODEL_NAME" ] || [ -z "$LOG_DIR" ]; then
 fi
 
 # == construct job launch command == 
+
+# create base job launch command 
 export LAUNCH_CMD="git clone https://github.com/hosseinsarshar/dist-training-vertex.git &&"
+
+# add checkpoint transfer to launch command # NOTE: set BUCKET env var before calling launch.sh
+if [ $TRAIN_TYPE="continual-pretraining" ] || [ $TRAIN_TYPE="continual-pretraining" ]; then
+    export CONVERTED_MODEL_PATH="/workspace/converted_models/$MODEL_NAME.nemo"
+    export LAUNCH_CMD="$LAUNCH_CMD && gsutil -m cp $GCS_PATH_TO_CKPT $CONVERTED_MODEL_PATH &&"
+    export ADDITIONAL_ARGS="$ADDITIONAL_ARGS ++model.resume_from_checkpoint=$CONVERTED_MODEL_PATH"
+fi
+
+# if in debug mode add sleep infinity to launch command
 if [ -z "$DEBUG" ]; then
     export LAUNCH_CMD="$LAUNCH_CMD chmod +x ./dist-training-vertex/nemo/job.sh && ./dist-training-vertex/nemo/job.sh"
 else 
@@ -45,7 +56,6 @@ else
 fi
 
 # == set job specific parameters based on model ==
-
 if [ $MODEL_NAME='llama3-70b' ]; then
     export NNODES=8
     export MICRO_BATCH=2
@@ -57,11 +67,6 @@ fi
 # == define additional args ==
 export ADDITIONAL_ARGS="++model.micro_batch_size=$MICRO_BATCH ++trainer.max_steps=2 ++trainer.limit_val_batches=0.0 ++trainer.val_check_interval=1"
 
-# == if starting from trained weights, define nemo checkpoint ==
-if [ $TRAIN_TYPE="continual-pretraining" ]; then
-    export CONVERTED_MODEL_PATH="/workspace/converted_models/$MODEL_NAME.nemo"
-    export ADDITIONAL_ARGS="$ADDITIONAL_ARGS ++model.resume_from_checkpoint=$CONVERTED_MODEL_PATH"
-fi
 # == create json stucture with existing environment variables ==
 json_job=$(envsubst < vertex-payload.json)
 
